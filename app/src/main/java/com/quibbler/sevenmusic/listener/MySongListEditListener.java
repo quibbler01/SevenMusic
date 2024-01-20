9719e11a48a16fd0e152c83a53a581083bc1311d
quibbler01 <fluent0418@gmail.com>
2024-01-20 23:08:36 +0800

    Singles editing monitor, adapter details page editing, main page update display

---
+++ app/src/main/java/com/quibbler/sevenmusic/listener/MySongListEditListener.java
@@ -0,0 +1,12 @@
+package com.quibbler.sevenmusic.listener;
+
+/**
+ * Package:        com.quibbler.sevenmusic.listener
+ * ClassName:      MySongListEditListener
+ * Description:    歌单编辑监听器，适配器/详情页面编辑，主页面更新显示
+ * Author:         zhaopeng
+ * CreateDate:     2019/9/21 14:44
+ */
+public interface MySongListEditListener {
+    public void showView(boolean show);
+}
