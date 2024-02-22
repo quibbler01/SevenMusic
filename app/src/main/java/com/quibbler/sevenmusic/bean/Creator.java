84b13335da53dfbec586a642378676beba305f1c
quibbler01 <fluent0418@gmail.com>
2024-02-23 00:10:48 +0800

    Discovery page. Found Fragment class, hosted by Main Activity

---
+++ app/src/main/java/com/quibbler/sevenmusic/bean/Creator.java
@@ -0,0 +1,21 @@
+package com.quibbler.sevenmusic.bean;
+
+/**
+ * Package:        com.quibbler.sevenmusic.bean
+ * ClassName:      Creator
+ * Description:    歌单创建者的Bean（考虑是否与User同用）
+ * Author:         yanwuyang
+ * CreateDate:     2019/9/24 11:23
+ */
+public class Creator {
+    private String userId;
+    private String nickname;
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public String getNickname() {
+        return nickname;
+    }
+}
