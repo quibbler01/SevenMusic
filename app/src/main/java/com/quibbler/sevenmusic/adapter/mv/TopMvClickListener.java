0ad2621eb27194e2afecdcfcb0faaf918deb1937
quibbler01 <fluent0418@gmail.com>
2025-10-29 22:31:41 +0800

    The callback for parsing the QR code result can customize the behavior after successful scanning

--- app/src/main/java/com/quibbler/sevenmusic/adapter/mv/TopMvClickListener.java
+++
@@ -1,8 +0,0 @@
-package com.quibbler.sevenmusic.adapter.mv;
-
-import com.quibbler.sevenmusic.bean.mv.MvInfo;
-
-public interface TopMvClickListener {
-    void onStartMvPlayActivity(MvInfo mvInfo);
-    void onClickMoreButton(MvInfo mvInfo);
-}
--- app/src/main/java/com/quibbler/sevenmusic/interfaces/ScrollScreenInterface.java
+++
@@ -1,8 +0,0 @@
-package com.quibbler.sevenmusic.interfaces;
-
-public interface ScrollScreenInterface {
-    /**
-     * 屏幕滑动到屏幕外侧的处理方法
-     */
-    void onScreenScrolledToEnd();
-}
