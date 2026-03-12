1b916c41c3017a5cf4759f5d724b4c1514c9bfe4
quibbler01 <fluent0418@gmail.com>
2026-03-12 22:10:08 +0800

    PreviewCallback

--- libzxing/src/main/java/com/google/zxing/decoding/InactivityTimer.kt
+++ libzxing/src/main/java/com/google/zxing/decoding/InactivityTimer.kt
@@ -13,61 +13,59 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+package com.google.zxing.decoding
 
-package com.google.zxing.decoding;
-
-import android.app.Activity;
-
-import com.google.zxing.decoding.FinishListener;
-
-import java.util.concurrent.Executors;
-import java.util.concurrent.ScheduledExecutorService;
-import java.util.concurrent.ScheduledFuture;
-import java.util.concurrent.ThreadFactory;
-import java.util.concurrent.TimeUnit;
+import android.app.Activity
+import java.util.concurrent.Executors
+import java.util.concurrent.ScheduledExecutorService
+import java.util.concurrent.ScheduledFuture
+import java.util.concurrent.ThreadFactory
+import java.util.concurrent.TimeUnit
 
 /**
  * Finishes an activity after a period of inactivity.
  */
-public final class InactivityTimer {
+class InactivityTimer(private val activity: Activity?) {
+    private val inactivityTimer: ScheduledExecutorService =
+        Executors.newSingleThreadScheduledExecutor(
+            DaemonThreadFactory()
+        )
+    private var inactivityFuture: ScheduledFuture<*>? = null
 
-    private static final int INACTIVITY_DELAY_SECONDS = 5 * 60;
-
-    private final ScheduledExecutorService inactivityTimer =
-            Executors.newSingleThreadScheduledExecutor(new DaemonThreadFactory());
-    private final Activity activity;
-    private ScheduledFuture<?> inactivityFuture = null;
-
-    public InactivityTimer(Activity activity) {
-        this.activity = activity;
-        onActivity();
+    init {
+        onActivity()
     }
 
-    public void onActivity() {
-        cancel();
-        inactivityFuture = inactivityTimer.schedule(new FinishListener(activity),
-                INACTIVITY_DELAY_SECONDS,
-                TimeUnit.SECONDS);
+    fun onActivity() {
+        cancel()
+        inactivityFuture = inactivityTimer.schedule(
+            FinishListener(activity),
+            INACTIVITY_DELAY_SECONDS.toLong(),
+            TimeUnit.SECONDS
+        )
     }
 
-    private void cancel() {
+    private fun cancel() {
         if (inactivityFuture != null) {
-            inactivityFuture.cancel(true);
-            inactivityFuture = null;
+            inactivityFuture!!.cancel(true)
+            inactivityFuture = null
         }
     }
 
-    public void shutdown() {
-        cancel();
-        inactivityTimer.shutdown();
+    fun shutdown() {
+        cancel()
+        inactivityTimer.shutdown()
     }
 
-    private static final class DaemonThreadFactory implements ThreadFactory {
-        public Thread newThread(Runnable runnable) {
-            Thread thread = new Thread(runnable);
-            thread.setDaemon(true);
-            return thread;
+    private class DaemonThreadFactory : ThreadFactory {
+        override fun newThread(runnable: Runnable?): Thread {
+            val thread = Thread(runnable)
+            thread.setDaemon(true)
+            return thread
         }
     }
 
+    companion object {
+        private val INACTIVITY_DELAY_SECONDS = 5 * 60
+    }
 }
