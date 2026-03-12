6c8004ff4ddefeae45b4162e96122d91e288da24
quibbler01 <fluent0418@gmail.com>
2026-03-12 22:10:05 +0800

    PreviewCallback

--- libzxing/src/main/java/com/google/zxing/camera/PreviewCallback.kt
+++ libzxing/src/main/java/com/google/zxing/camera/PreviewCallback.kt
@@ -1,44 +1,39 @@
+package com.google.zxing.camera
 
-package com.google.zxing.camera;
+import android.hardware.Camera
+import android.os.Handler
+import android.util.Log
 
-import android.graphics.Point;
-import android.hardware.Camera;
-import android.os.Handler;
-import android.os.Message;
-import android.util.Log;
+class PreviewCallback internal constructor(
+    private val configManager: CameraConfigurationManager,
+    private val useOneShotPreviewCallback: Boolean
+) : Camera.PreviewCallback {
+    private var previewHandler: Handler? = null
+    private var previewMessage = 0
 
-public final class PreviewCallback implements Camera.PreviewCallback {
-
-    private static final String TAG = PreviewCallback.class.getSimpleName();
-
-    private final CameraConfigurationManager configManager;
-    private final boolean useOneShotPreviewCallback;
-    private Handler previewHandler;
-    private int previewMessage;
-
-    PreviewCallback(CameraConfigurationManager configManager, boolean useOneShotPreviewCallback) {
-        this.configManager = configManager;
-        this.useOneShotPreviewCallback = useOneShotPreviewCallback;
+    fun setHandler(previewHandler: Handler?, previewMessage: Int) {
+        this.previewHandler = previewHandler
+        this.previewMessage = previewMessage
     }
 
-    public void setHandler(Handler previewHandler, int previewMessage) {
-        this.previewHandler = previewHandler;
-        this.previewMessage = previewMessage;
-    }
-
-    public void onPreviewFrame(byte[] data, Camera camera) {
-        Point cameraResolution = configManager.getCameraResolution();
+    override fun onPreviewFrame(data: ByteArray?, camera: Camera) {
+        val cameraResolution = configManager.getCameraResolution()
         if (!useOneShotPreviewCallback) {
-            camera.setPreviewCallback(null);
+            camera.setPreviewCallback(null)
         }
         if (previewHandler != null) {
-            Message message = previewHandler.obtainMessage(previewMessage, cameraResolution.x,
-                    cameraResolution.y, data);
-            message.sendToTarget();
-            previewHandler = null;
+            val message = previewHandler!!.obtainMessage(
+                previewMessage, cameraResolution.x,
+                cameraResolution.y, data
+            )
+            message.sendToTarget()
+            previewHandler = null
         } else {
-            Log.d(TAG, "Got preview callback, but no handler for it");
+            Log.d(TAG, "Got preview callback, but no handler for it")
         }
     }
 
+    companion object {
+        private val TAG: String = PreviewCallback::class.java.getSimpleName()
+    }
 }
