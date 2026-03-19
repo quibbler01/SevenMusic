f5149238f54bb2d357dd6be6d9e96038415bddb3
quibbler01 <fluent0418@gmail.com>
2026-03-19 23:43:17 +0800

    context Context

--- libzxing/src/main/java/com/google/zxing/NetworkUtil.kt
+++ libzxing/src/main/java/com/google/zxing/NetworkUtil.kt
@@ -1,31 +1,28 @@
-package com.google.zxing;
+package com.google.zxing
 
-import android.content.Context;
-import android.net.ConnectivityManager;
-import android.net.NetworkInfo;
+import android.content.Context
+import android.net.ConnectivityManager
+import android.net.NetworkInfo
 
-public class NetworkUtil {
-
+object NetworkUtil {
     /**
      * 检测当的网络（WLAN、3G/2G）状态
      * @param context Context
      * @return true 表示网络可用
      */
-    public static boolean isNetworkAvailable(Context context) {
-        ConnectivityManager connectivity = (ConnectivityManager) context
-                .getSystemService(Context.CONNECTIVITY_SERVICE);
+    fun isNetworkAvailable(context: Context): Boolean {
+        val connectivity = context
+            .getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager?
         if (connectivity != null) {
-            NetworkInfo info = connectivity.getActiveNetworkInfo();
-            if (info != null && info.isConnected())
-            {
+            val info = connectivity.getActiveNetworkInfo()
+            if (info != null && info.isConnected()) {
                 // 当前网络是连接的
-                if (info.getState() == NetworkInfo.State.CONNECTED)
-                {
+                if (info.getState() == NetworkInfo.State.CONNECTED) {
                     // 当前所连接的网络可用
-                    return true;
+                    return true
                 }
             }
         }
-        return false;
+        return false
     }
 }
