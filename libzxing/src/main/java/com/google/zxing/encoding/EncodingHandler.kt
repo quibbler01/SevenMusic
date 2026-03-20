b0b45cab0ec9a24817bf0fcc3e036a8d44df2453
quibbler01 <fluent0418@gmail.com>
2026-03-20 23:08:04 +0800

    context Context

--- libzxing/src/main/java/com/google/zxing/encoding/EncodingHandler.kt
+++ libzxing/src/main/java/com/google/zxing/encoding/EncodingHandler.kt
@@ -1,37 +1,39 @@
-package com.google.zxing.encoding;
+package com.google.zxing.encoding
 
-import android.graphics.Bitmap;
+import android.graphics.Bitmap
+import com.google.zxing.BarcodeFormat
+import com.google.zxing.EncodeHintType
+import com.google.zxing.MultiFormatWriter
+import com.google.zxing.WriterException
+import java.util.Hashtable
 
-import com.google.zxing.BarcodeFormat;
-import com.google.zxing.EncodeHintType;
-import com.google.zxing.MultiFormatWriter;
-import com.google.zxing.WriterException;
-import com.google.zxing.common.BitMatrix;
+object EncodingHandler {
+    private const val BLACK = -0x1000000
 
-import java.util.Hashtable;
+    @Throws(WriterException::class)
+    fun createQRCode(str: String?, widthAndHeight: Int): Bitmap {
+        val hints = Hashtable<EncodeHintType?, String?>()
+        hints.put(EncodeHintType.CHARACTER_SET, "utf-8")
+        val matrix = MultiFormatWriter().encode(
+            str,
+            BarcodeFormat.QR_CODE, widthAndHeight, widthAndHeight
+        )
+        val width = matrix.getWidth()
+        val height = matrix.getHeight()
+        val pixels = IntArray(width * height)
 
-public final class EncodingHandler {
-    private static final int BLACK = 0xff000000;
-
-    public static Bitmap createQRCode(String str, int widthAndHeight) throws WriterException {
-        Hashtable<EncodeHintType, String> hints = new Hashtable<EncodeHintType, String>();
-        hints.put(EncodeHintType.CHARACTER_SET, "utf-8");
-        BitMatrix matrix = new MultiFormatWriter().encode(str,
-                BarcodeFormat.QR_CODE, widthAndHeight, widthAndHeight);
-        int width = matrix.getWidth();
-        int height = matrix.getHeight();
-        int[] pixels = new int[width * height];
-
-        for (int y = 0; y < height; y++) {
-            for (int x = 0; x < width; x++) {
+        for (y in 0..<height) {
+            for (x in 0..<width) {
                 if (matrix.get(x, y)) {
-                    pixels[y * width + x] = BLACK;
+                    pixels[y * width + x] = BLACK
                 }
             }
         }
-        Bitmap bitmap = Bitmap.createBitmap(width, height,
-                Bitmap.Config.ARGB_8888);
-        bitmap.setPixels(pixels, 0, width, 0, 0, width, height);
-        return bitmap;
+        val bitmap = Bitmap.createBitmap(
+            width, height,
+            Bitmap.Config.ARGB_8888
+        )
+        bitmap.setPixels(pixels, 0, width, 0, 0, width, height)
+        return bitmap
     }
 }
